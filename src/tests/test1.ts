f56f50d40923d918668dec6fc155cefdfc10bef3
zui-situ <382808911@qq.com>
2022-04-26 16:22:23 +0800

    chore(修改测试用例): build(修改测试文档名): test(删除测试jest文件): test(deps): 修改测试文件

--- src/tests/test1.ts
+++ src/tests/test1.ts
@@ -1,4 +1,10 @@
-const A = 'hello'
+import { mount } from '@vue/test-utils'
+import Test from '../src/views/search/index.vue'
 
-// eslint-disable-next-line no-console
-console.log(A)
+test('Test.vue', async () => {
+  const wrapper = mount(Test)
+  expect(wrapper.html()).toContain('Unit Test Page')
+  expect(wrapper.html()).toContain('count is: 0')
+  await wrapper.find('button').trigger('click')
+  expect(wrapper.html()).toContain('count is: 1')
+})
