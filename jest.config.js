e14aead807c063d86f9bed03f2588c19f1c6f6fb
zui-situ <382808911@qq.com>
2022-04-26 16:02:10 +0800

    test(deps): 修改测试文件

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,10 @@ module.exports = {
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '^.+\\.ts$': 'ts-jest'
-  },
-  // 匹配 __tests__ 目录下的 .js/.ts 文件 或 xx.test.js/ts xx.spec.js/ts
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1' // 配置 jest 下 @ -> src
   }
+  // 匹配 __tests__ 目录下的 .js/.ts 文件 或 xx.test.js/ts xx.spec.js/ts
+  // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
+  // moduleNameMapper: {
+  //   '^@/(.*)$': '<rootDir>/src/$1' // 配置 jest 下 @ -> src
+  // }
 }
